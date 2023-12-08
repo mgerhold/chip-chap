c0f391c8f022f6d09022636d1f782d6583f06a64
Michael Gerhold <michael.gerhold@web.de>
2023-12-08 23:53:28 +0100

    fixed bug regarding XOR drawing

--- src/chip_chap/screen.cpp
+++ src/chip_chap/screen.cpp
@@ -1,14 +1,17 @@
 #include "screen.hpp"
 #include <cstring>
 
+static constexpr auto visible = 0xFFFFFFFF;
+static constexpr auto invisible = 0x000000FF;
+
 bool Screen::set_pixel(u8 const x, u8 const y, bool is_set) {
     auto const previous_value = get_pixel(x, y);
-    m_pixels.at(coordinates_to_offset(x, y)) = (is_set ? 0xFFFFFFFF : 0x000000FF);
+    m_pixels.at(coordinates_to_offset(x, y)) = (is_set ? visible : invisible);
     return previous_value;
 }
 
 [[nodiscard]] bool Screen::get_pixel(u8 const x, u8 const y) const {
-    return m_pixels.at(coordinates_to_offset(x, y)) != 0;
+    return m_pixels.at(coordinates_to_offset(x, y)) == visible;
 }
 
 [[nodiscard]] usize Screen::width() const {
