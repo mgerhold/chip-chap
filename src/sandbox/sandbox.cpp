d655c065a22d9b608de53a4ed31cf481ead94764
Michael Gerhold <michael.gerhold@web.de>
2023-12-19 21:39:06 +0100

    restructured sandbox

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -1,5 +1,14 @@
 #include <chissembler/chissembler.hpp>
+#include <string_view>
 
 int main() {
-    auto machine_code = chissembler::assemble("stdin", "copƒ o2");
+    using namespace std::string_view_literals;
+    static constexpr auto source = R"(start:
+one_more:
+    copy 42 V0
+    copy V0 V1
+later:
+    add 1 V1
+)"sv;
+    auto machine_code = chissembler::assemble("stdin", source);
 }
